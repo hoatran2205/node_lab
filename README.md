85cf813aabc06a098f130cb6afccc80d455f6103
huybinhvn <67670004+huybinhvn@users.noreply.github.com>
2022-09-26 15:04:14 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# node_test# node_lab
---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const os = require('os');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send(
+    `<body style='background-color:#283E5B'><h1 style='color: orange;text-align:center'>Ben try to hello AWS from ECS Fragate ${os.hostname()}</h1></body>`
+  );
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
